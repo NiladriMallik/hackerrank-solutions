e3f5d546b3d64d5b9fbdc63326d03e1a6e179572
Niladri Mallik <51795733+NiladriMallik@users.noreply.github.com>
2024-04-09 18:57:28 +0530

    Update TheReport.sql

--- SQL/TheReport.sql
+++ SQL/TheReport.sql
@@ -1,21 +1,23 @@
 with MarksTable as( 
 select
-st.name,
-gr.grade,
-st.marks
+    st.name,
+    gr.grade,
+    st.marks
 from students st
-join
-grades gr
+    join grades gr
 on st.marks between gr.min_mark and gr.max_mark
--- where gr.grade >= 8
--- order by gr.grade desc, name asc
 )
 select 
-case
-    when grade <8 then "NULL"
-    else name
-end as name,
-grade,
-marks
+    name,
+    grade,
+    marks
 from MarksTable
+where grade >= 8
+    union
+select 
+    'NULL' as name,
+    grade,
+    marks
+from MarksTable
+where grade < 8
 order by grade desc, name asc;
